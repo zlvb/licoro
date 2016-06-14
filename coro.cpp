c78e391dc36e98498f43ff1256a27ca87bb47edd
zlvb <zlvbvbzl@gmail.com>
2016-06-14 21:35:29 +0800

    修改

--- coro.cpp
+++ coro.cpp
@@ -1,12 +1,13 @@
 #include "coro.h"
+#include "common/Log.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ucontext.h>
 #include <assert.h>
 #include <stddef.h>
 #include <string.h>
-#include <deque>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <setjmp.h>
 #include <tr1/unordered_map>
@@ -30,93 +31,7 @@ struct coro_handle
 };
 
 typedef std::tr1::unordered_map<int64_t, coro_handle*> CoroMap;
-
-template <class T>
-class VectorBase
-{
-protected:
-    typedef typename std::deque<T> CntType;
-public:
-    typedef typename CntType::iterator iterator;
-    size_t size() const
-    {
-        return m_data.size();
-    }
-
-    iterator begin()
-    {
-        return m_data.begin();
-    }
-
-    iterator end()
-    {
-        return m_data.end();
-    }
-
-    void clear()
-    {
-        m_data.clear();
-    }
-
-    bool empty() const
-    {
-        return m_data.empty();
-    }
-
-    void push(const T &v)
-    {
-        m_data.push_back(v);
-    }
-
-protected:
-    CntType m_data;
-};
-
-struct sleep_compare
-{
-    bool operator()(const std::pair<int64_t, int64_t>& x, const std::pair<int64_t, int64_t>& y) const
-    {
-        return x.first < y.first;
-    }
-};
-
-template <class T, class Compare>
-class OrderedVector : public VectorBase<T>
-{
-public:
-    typedef typename VectorBase<T>::iterator iterator;
-    iterator begin() { return VectorBase<T>::m_data.begin(); }
-    iterator end() { return VectorBase<T>::m_data.end(); }
-
-    void add(const T& t)
-    {
-        iterator i = std::lower_bound(begin(), end(), t, cmp);
-        if (i == end() || cmp(t, *i))
-            VectorBase<T>::m_data.insert(i, t);
-    }
-    void remove(int idx)
-    {
-        VectorBase<T>::m_data.erase(begin() + idx);
-    }
-
-    void erase(const T &value)
-    {
-        auto bounds = std::equal_range(begin(), end(), value);
-        auto last = end() - std::distance(bounds.first, bounds.second);
-        std::swap_ranges(bounds.first, bounds.second, last);
-        VectorBase<T>::m_data.erase(last, end());
-    }
-
-    T &operator[](size_t idx)
-    {
-        return VectorBase<T>::m_data[idx];
-    }
-
-private:
-    Compare cmp;
-};
-
-typedef OrderedVector<std::pair<int64_t, int64_t>, sleep_compare> SleepList;
+typedef std::map<int64_t, int64_t> SleepList;
 
 static ucontext_t uctx_main;
 static ucontext_t *gs_uctx_current = &uctx_main;
@@ -126,27 +41,18 @@ static SleepList gs_SleepList;
 static int64_t gs_id = 1;
 static CoroMap gs_coromap;
 static size_t gs_maxcorocount = 0;
-static size_t gs_finished = 0;
-static size_t gs_timeout = 0;
-static const struct INITFinish
-{
-    INITFinish()
-    {
-        printf("coro init finish\n");
-    }
-}gs_fi;
 
 static void coro_agent(uint32_t low32, uint32_t hi32)
 {
     uintptr_t ptr = (uintptr_t)low32 | ((uintptr_t)hi32 << 32);
     coro_handle *hcoro = (coro_handle*)ptr;
-    hcoro->func(hcoro->custom_data);
-    gs_FreeList.push_back(hcoro);
-    gs_uctx_current = &uctx_main;
+    hcoro->func(hcoro->custom_data);    
+    gs_FreeList.push_back(hcoro);  
+    gs_uctx_current = hcoro->uctx.uc_link;
     gs_current_handle = NULL;
-    gs_finished++;
     if (0 == _setjmp(hcoro->jmpbuf))
     {
+		LOG_DEBUG("CORO out: %p %ld cur=%p", hcoro, hcoro->id, gs_current_handle);
         _longjmp(hcoro->last_jmpbuf, 1);
     }
 }
@@ -171,13 +77,14 @@ coro_handle *coro_new( corofunc coro, void *custom_data, uint32_t stack_size )
 
 static void coro_del( coro_handle *hcoro )
 {
+	LOG_DEBUG("CORO DEL: %p %ld cur=%p", hcoro, hcoro->id, gs_current_handle);
     gs_coromap.erase(hcoro->id);
     free(hcoro->stack_mem);
     free(hcoro);
 }
 
 int coro_resume_at(coro_handle *hcoro, int64_t wake_time)
-{
+{	
     if (!hcoro->started)
     {
         if (&hcoro->uctx == gs_uctx_current)
@@ -186,6 +93,8 @@ int coro_resume_at(coro_handle *hcoro, int64_t wake_time)
 
     if (wake_time > 0)
     {
+		hcoro->sleepsec = wake_time;
+		gs_SleepList.insert(std::make_pair(hcoro->sleepsec, hcoro->id));
         return CORO_OK;
     }
 
@@ -194,34 +103,48 @@ int coro_resume_at(coro_handle *hcoro, int64_t wake_time)
         return CORO_ALREDY_RUN;
     }
 
+	coro_handle *old_hcoro = gs_current_handle;
     if (_setjmp(hcoro->last_jmpbuf) == 0)
-    {
+    {        
         if (!hcoro->started)
         {
-            getcontext(&hcoro->uctx);
+            getcontext(&hcoro->uctx);    
             hcoro->uctx.uc_stack.ss_sp = hcoro->stack_mem;
             hcoro->uctx.uc_stack.ss_size = hcoro->stack_size;
-            hcoro->uctx.uc_link = &uctx_main;
+            hcoro->uctx.uc_link = gs_uctx_current;
 
             uintptr_t ptr = (uintptr_t)hcoro;
-            makecontext(&hcoro->uctx, (void (*)(void))coro_agent, 2, (uint32_t)ptr, (uint32_t)(ptr >> 32));
-            hcoro->started = true;
+            makecontext(&hcoro->uctx, (void (*)(void))coro_agent, 2, (uint32_t)ptr, (uint32_t)(ptr >> 32));   
+            hcoro->started = true;            
             gs_current_handle = hcoro;
             ucontext_t *uctx_parent = gs_uctx_current;
             gs_uctx_current = &hcoro->uctx;
+			LOG_DEBUG("CORO in: %p %ld cur=%p", hcoro, hcoro->id, gs_current_handle);
             swapcontext(uctx_parent, &hcoro->uctx);
-        }
+        }   
         else
         {
             gs_current_handle = hcoro;
             if (gs_current_handle->sleepsec > 0)
             {
-                gs_SleepList.erase(std::make_pair(hcoro->sleepsec, hcoro->id));
+				auto it = gs_SleepList.find(hcoro->sleepsec);
+				for (; it->first == hcoro->sleepsec; ++it)
+				{
+					if (it->second == hcoro->id)
+					{
+						gs_SleepList.erase(it);
+						break;
+					}
+				}          
             }
             gs_current_handle->sleepsec = 0;
+			LOG_DEBUG("CORO in: %p %ld cur=%p", hcoro, hcoro->id, gs_current_handle);
             _longjmp(hcoro->jmpbuf, 1);
-        }
+        } 
     }
+
+	gs_current_handle = old_hcoro;
+	LOG_DEBUG("CORO out: %p %ld cur=%p", hcoro, hcoro->id, gs_current_handle);
     return CORO_OK;
 }
 
@@ -229,20 +152,23 @@ int coro_yield(int64_t wake_time)
 {
     if (!gs_current_handle)
         return CORO_NOT_IN_CORO;
-
+	
     if (wake_time > 0)
     {
         gs_current_handle->sleepsec = wake_time;
-        gs_SleepList.add(std::make_pair(gs_current_handle->sleepsec, gs_current_handle->id));
+        gs_SleepList.insert(std::make_pair(gs_current_handle->sleepsec, gs_current_handle->id));
     }
     else
     {
         gs_current_handle->sleepsec = TIME_INFINITY;
     }
+	
     if (0 == _setjmp(gs_current_handle->jmpbuf))
     {
+		LOG_DEBUG("CORO out: %p %ld cur=%p", gs_current_handle, gs_current_handle->id, gs_current_handle);
         _longjmp(gs_current_handle->last_jmpbuf, 1);
     }
+	LOG_DEBUG("CORO in: %p %ld cur=%p", gs_current_handle, gs_current_handle->id, gs_current_handle);
 
     if (gs_current_handle->sleepsec == TIME_TIMEOUT)
     {
@@ -264,7 +190,7 @@ int coro_resume_at(int64_t cid, int64_t wake_time)
 
 
 bool coro_schedule(const timeval &tv)
-{
+{    
     int64_t now = tv.tv_sec * 1000000 + tv.tv_usec;
     return coro_schedule(now);
 }
@@ -273,15 +199,16 @@ bool coro_schedule(int64_t now)
 {
     while (!gs_SleepList.empty())
     {
-        const auto &n = gs_SleepList[0];
-        if (now < n.first)
+        const auto &n = gs_SleepList.begin();
+        if (now < n->first)
             break;
 
-        auto it = gs_coromap.find(n.second);
-        gs_SleepList.remove(0);
-        if (it != gs_coromap.end() && it->second->sleepsec > 0)
+        auto it = gs_coromap.find(n->second);
+		int64_t waketime = n->first;
+        gs_SleepList.erase(n);		
+        if (it != gs_coromap.end() && it->second->sleepsec > 0 && it->second->sleepsec == waketime)
         {
-            gs_timeout++;
+			LOG_DEBUG("%ld %ld", it->second->sleepsec, waketime);
             it->second->sleepsec = TIME_TIMEOUT;
             int ret = coro_resume_at(it->second, 0);
             assert(ret == 0);
@@ -310,13 +237,11 @@ coro_handle *coro_get_current_handle()
     return gs_current_handle;
 }
 
-void coro_get_statistic(size_t *pMaxCoro, size_t *pSleepCoro, size_t *pTotal, size_t *pFinished, size_t *pTimeout)
+void coro_get_statistic(size_t *pMaxCoro, size_t *pSleepCoro, size_t *pTotal)
 {
-    *pSleepCoro = gs_SleepList.size();
+    *pSleepCoro = (int)gs_SleepList.size();
     *pTotal = gs_coromap.size();
     *pMaxCoro = gs_maxcorocount;
-    *pFinished = gs_finished;
-    *pTimeout = gs_timeout;
 }
 
 size_t coro_get_current_stack_used()
--- coro.h
+++ coro.h
@@ -1,16 +1,3 @@
-/**
-*
-* @file     coro.h
-* @brief    协程库头文件
-*
-* @author cppzhang <cppzhang@tencent.com>
-* @date 2014-5-7   17:02
-*
-*
-* Copyright (c)  2013-2014, Tencent
-* All rights reserved.
-*
-*/
 #ifndef coro_h__
 #define coro_h__
 
@@ -26,6 +13,7 @@
 // 协程句柄
 struct coro_handle;
 struct timeval;
+typedef int64_t CORO_ID;
 // 协程函数
 // custom_data      自定义数据
 typedef void (*corofunc)(void *custom_data);
@@ -40,13 +28,14 @@ coro_handle *coro_new(corofunc coro, void *custom_data = 0, uint32_t stack_size 
 // 切换到一个挂起的协程使之继续执行
 // hcoro 协程句柄
 int coro_resume_at(coro_handle *hcoro, int64_t wake_time);
-int coro_resume_at(int64_t id, int64_t wake_time);
+int coro_resume_at(CORO_ID id, int64_t wake_time);
 
 #define coro_resume(coro) coro_resume_at(coro, 0)
 
 // 挂起一个协程
-// wake_time       何时唤醒（微秒）
-int coro_yield(int64_t wake_time = 0);
+// sleep_time       唤醒的时刻（微秒）
+int coro_yield(int64_t wake_time);
+#define coro_yield_dangerous() coro_yield(0) // 永久挂起，除非主动唤醒
 
 // 协程调度函数
 // 返回是否还有协程等待调度
@@ -54,17 +43,19 @@ bool coro_schedule(const timeval &tv);
 bool coro_schedule(int64_t now);
 
 // 获得状态
-void coro_get_statistic(size_t *pMaxCoro, size_t *pSleepCoro, size_t *pTotal, size_t *pFinished, size_t *pTimeout);
+void coro_get_statistic(size_t *pMaxCoro, size_t *pSleepCoro, size_t *pTotal);
 
 // 获得当前栈大小
 size_t coro_get_current_stack_used();
 
 // 返回当前的协程id
-int64_t coro_getid();
+CORO_ID coro_getid();
 coro_handle *coro_get_current_handle();
 
 
 // 创建并开始一个协程
 #define coro_go(...) coro_resume(coro_new(__VA_ARGS__))
 
+#define SEC2WAKETIME(sec, tv_now) ((tv_now).tv_sec * 1000000 + (tv_now).tv_usec + (sec) * 1000000)
+#define MSEC2WAKETIME(msec, tv_now) ((tv_now).tv_sec * 1000000 + (tv_now).tv_usec + (msec) * 1000)
 #endif // coro_h__
